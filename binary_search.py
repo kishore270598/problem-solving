ca6e241571d6546a9a6ee7e4b429914e2ff423e8
kishore270598 <kk94323@gmail.com>
2023-08-01 23:49:10 +0530

    ceil,searchInsert

--- binary_search.py
+++ binary_search.py
@@ -4,15 +4,14 @@ class Binary_serach_prob:
     def search(self,arr,target):
         low=0
         high=len(arr)-1
-        for i in range(0,len(arr),1):
+        while(low<=high):
             mid=(low+high)//2
-            while(low<=high):
-                if(arr[mid]==target):
-                    return mid
-                elif(target>arr[mid]):
-                    low=mid+1
-                else:
-                    high=mid-1
+            if(arr[mid]==target):
+                return mid
+            elif(target>arr[mid]):
+                low=mid+1
+            else:
+                high=mid-1
     def search_op(self,arr,target):
         return self.search_rec(arr,0,len(arr)-1,target)
     
@@ -54,12 +53,66 @@ class Binary_serach_prob:
             else:
                 low=mid+1
         return value
+    # You are given a sorted array arr of distinct values and a target value . You need to search for the index of the target value in the array.
+    # if already there is target value just return that index 
+    def searchInsert(self, arr, target):
+        return self.search_rec(arr,0,len(arr)-1,target)
+    
+    def search_rec(self,arr,low,high,target):
+        if(low>high):
+            return ((low+high)//2)+1 #( return the expected place where a target can be)
+        mid=(low+high)//2
+        if(arr[mid]==target):
+            return mid #(already target is there)
+        elif(target>arr[mid]):
+            return self.search_rec(arr,mid+1,high,target)
+        else:
+            return self.search_rec(arr,low,mid-1,target)
+    
+
+    def ceilingInSortedArray(self,arr,target):
+        arr=sorted(arr)
+        print(arr)
+        floor=self.binary_search_floor(arr,target)
+        ceil=self.binary_search_ceil(arr,target)
+        return [floor,ceil]
+
+    
+    def binary_search_floor(self,arr,target):
+        low=0
+        high=len(arr)-1
+        flr=-1
+        while(low<=high):
+            mid=(low+high)//2
+            if(arr[mid]==target):
+                return arr[mid]
+            elif(arr[mid]>target):
+                high=mid-1
+            else:
+                flr=arr[mid]
+                low=mid+1
+        return flr
+    
+    def binary_search_ceil(self,arr,target):
+        low=0
+        high=len(arr)-1
+        cel=-1
+        while(low<=high):
+            mid=(low+high)//2
+            if(arr[mid]==target):
+                return arr[mid]
+            elif(arr[mid]>target):
+                cel=arr[mid]
+                high=mid-1
+            else:
+                low=mid+1
+        return cel
 
 a=Binary_serach_prob()
 arr=[]
-arr1=[1, 2, 2, 3]
+arr1=[3 ,44, 14, 39 ,41, 47 ,37 ,1, 21 ]
 arr2=[1]
-print(a.lowerBound(arr1,2))
+print(a.ceilingInSortedArray(arr1,35))
 # for test in arr:
 #     start = time.time()
 #     print('Input',test)
