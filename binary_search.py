c2768761dd3c4ab825d4314ca11ba50f38930313
kishore270598 <kk94323@gmail.com>
2023-08-17 11:31:53 +0530

    kthElement in 2 sorted array,findMedian in sorted array

--- binary_search.py
+++ binary_search.py
@@ -613,13 +613,118 @@ class Binary_serach_prob:
             gas+=numberinbetween
         return gas
     
+    def findMedianSortedArrays(self, arr1, arr2):
+        n1=len(arr1)
+        n2=len(arr2)
+        arr3=[]
+        i=0
+        j=0
+        while(i<n1 and j<n2):
+            if(arr1[i]<arr2[j]):
+                arr3.append(arr1[i])
+                i+=1
+            else:
+                arr3.append(arr2[j])
+                j+=1
+        
+        while(i<n1):
+            arr3.append(arr1[i])
+            i+=1
+        while(j<n2):
+            arr3.append(arr2[j])
+            j+=1
+
+        n=n1+n2
+        if(n%2==1):
+            return arr3[n//2]
+        return ((arr3[n//2]+arr3[(n//2)-1])/2.0)
+    
+
+    def findMedianSortedArrays_opt(self, arr1, arr2):
+        n1=len(arr1)
+        n2=len(arr2)
+        n=n1+n2
+        if(n1>n2):
+            return self.findMedianSortedArrays_opt(arr2,arr1)
+        left=(n1+n2+1)//2
+        low=0
+        high=n1
+
+        while(low<=high):
+            mid1=(low+high)//2
+            mid2=left-mid1
+            l1=0
+            l2=0
+            r1=99999999999999
+            r2=99999999999999
+            if(mid1<n1):
+                r1=arr1[mid1]
+            if(mid2<n2):
+                r2=arr2[mid2]
+            if(mid1-1>=0):
+                l1=arr1[mid1-1]
+            if(mid2-1>=0):
+                l2=arr2[mid2-1]
+            if(l1<=r2 and l2<=r1):
+                if(n%2==1):
+                    return max(l1,l2)
+                return (float(max(l1,l2)) +float(min(r1,r2)))/2.0
+            elif(l1>r2):
+                high=mid1-1
+            else:
+                low=mid1+1
+        return 0
+    
+    
+    def kthElement(self,arr1, n, arr2, m, k):
+        n1=len(arr1)
+        n2=len(arr2)
+        n=n1+n2
+        if(n1>n2):
+            return self.findMedianSortedArrays_opt(arr2,m,arr1,n,k)
+        left=k
+        low=max(k-n2,0)
+        high=min(k,n1)
+
+        while(low<=high):
+            mid1=(low+high)//2
+            mid2=left-mid1
+            l1=0
+            l2=0
+            r1=99999999999999
+            r2=99999999999999
+            if(mid1<n1):
+                r1=arr1[mid1]
+            if(mid2<n2):
+                r2=arr2[mid2]
+            if(mid1-1>=0):
+                l1=arr1[mid1-1]
+            if(mid2-1>=0):
+                l2=arr2[mid2-1]
+            if(l1<=r2 and l2<=r1):
+                if(n%2==1):
+                    return max(l1,l2)
+                return (float(max(l1,l2)) +float(min(r1,r2)))/2.0
+            elif(l1>r2):
+                high=mid1-1
+            else:
+                low=mid1+1
+        return 0
+
+
+
+
+
+
+
+
 
 
 a=Binary_serach_prob()
 arr=[1, 2, 3, 4, 5]
-arr1=[1]
-arr2=[1]
-print(a.minimiseMaxDistance(arr,4))
+arr1=[1,3]
+arr2=[2]
+print(a.findMedianSortedArrays_opt(arr1,arr2))
 # for test in arr:
 #     start = time.time()
 #     print('Input',test)
