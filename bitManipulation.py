d3feed911d075bd19aa37307e730165284aedbe3
kishore270598 <kk94323@gmail.com>
2023-09-23 00:15:59 +0530

    countSetBits,swap,set bit

--- bitManipulation.py
+++ bitManipulation.py
@@ -15,12 +15,61 @@ class Bitmanipulation():
               return 'even'
          else:
               return 'odd'
-         
-
-
-
+    def isPowerOfTwo(self, x: int) -> bool:
+        if((n & (n-1))==0):
+            return True
+        else:
+            return False
+    def countSetBits(self,N: int) -> int:
+        count=0
+        K=N
+        for i in range(1,K+1,1):
+            #010 * 1 -- 0 # right shift001 * 1 -1 count-1
+            while(i!=0):
+                if((i & 1) ==1):
+                    count+=1
+                i=i>>1
+        return count
+    #even (x//2=y)
+    # no of set bits of x will be equal to no of set bits of y
+    # odd 
+    # no of set bits of x will =  1+ no of set bit of y since we divide the 2 (right shift we lose the 1 )  
+    def git (self,N: int) -> int:
+        count=[0]*(N+1)
+        count[0]=0
+        for i in range(1,N+1,1):
+            count[i]=count[i//2] +i%2
+        return sum(count) 
+    def countSetBits(self,N: int) -> int:
+        ans=0
+        d=2
+        x=N
+        while(x):
+            ans+=((N+1)//d)*(d//2) + max((N+1)%d-d//2,0)
+            d*=2
+            x/=2
+        return ans     
+    def setBits(self,N):    
+        temp=N
+        x=1
+        flag=1
+        while(temp!=0):
+            if((N & x)==0):
+                flag=0
+                break
+            x=x<<1
+            temp=temp>>1
+        if(flag==0):
+            return N+x
+        else:
+            return N
+    def swapNumber(self,a,  b):
+        a=a^b
+        b=a^b
+        a=a^b
+        return a,b
 
 
 b=Bitmanipulation()
-ans=b.bitManipulation(128,7)
+ans=b.swapNumber(3,2)
 print(ans)
