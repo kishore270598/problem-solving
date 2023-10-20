037e5a77b7f5eb8abbb4e8943acd6c448c0cabc5
kishore270598 <kk94323@gmail.com>
2023-10-21 00:44:32 +0530

    LFU

--- stack.py
+++ stack.py
@@ -1,6 +1,7 @@
 import time
 import math
 from collections import deque 
+from collections import defaultdict
 class Stack:
     def __init__(self, capacity: int):
         self.stack=[0]*capacity
@@ -741,23 +742,57 @@ class lruCache:
                 self.cache.remove(key)  #remove the old 
                 self.dicty[key] = value 
                 self.cache.appendleft(key) #make it as recently used
+
+from collections import defaultdict, OrderedDict
+
+class LFUCache:
+
+    def __init__(self, capacity: int):
+        # Initialize the cache, frequencies, and minimum frequency
+        self.capacity = capacity
+        self.cache = {}
+        self.frequencies = defaultdict(OrderedDict)
+        self.min_freq = 1
+
+    def _update(self, key: int):
+        # Update the frequency of the key and its position in the frequencies dict
+        freq = self.cache[key][1]
+        self.frequencies[freq].pop(key)#to pop the key and its frequecny
         
+        if not self.frequencies[freq]:
+            self.frequencies.pop(freq)
+            if self.min_freq == freq:
+                self.min_freq += 1
+        
+        self.cache[key] = (self.cache[key][0], freq + 1)
+        self.frequencies[freq + 1][key] = None
 
+    def get(self, key: int) -> int:
+        # If the key is not in the cache, return -1
+        if key not in self.cache:
+            return -1
 
+        # Update the frequency and position of the key
+        self._update(key)
 
+        # Return the value associated with the key
+        return self.cache[key][0]
 
-   
+    def put(self, key: int, value: int) -> None:
+        if self.capacity == 0:
+            return
 
-
-    
-
-
-
-
-
-    
-         
-
+        if key in self.cache:
+            self._update(key)
+            self.cache[key] = (value, self.cache[key][1])
+        else:
+            if len(self.cache) == self.capacity:
+                least_frequent_key, _ = self.frequencies[self.min_freq].popitem(last=False)
+                self.cache.pop(least_frequent_key)
+            
+            self.cache[key] = (value, 1)
+            self.frequencies[1][key] = None
+            self.min_freq = 1
 
 
             
