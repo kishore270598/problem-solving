a731302a063515bb1a472e4225aed2b9519c663e
kishore270598 <kk94323@gmail.com>
2024-01-02 00:29:46 +0530

    Insert a  node in binary tree ,delete a node

--- Binary_search_tree.py
+++ Binary_search_tree.py
@@ -28,6 +28,14 @@ class Binary_search_tree:
             return root
         else:
             return None
+    def minVal(self,root):
+        prev=0
+        if root:
+            while root.left:
+                root=root.left
+            return root.data
+        else:
+            return -1
     def findCeil(root, x):
         ceil=-1
         while root:
@@ -54,4 +62,81 @@ class Binary_search_tree:
                 root=root.left
         return floor
     
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        #check the nearest value
+        if root is None:
+            node=TreeNode(val,None,None)
+            return node
+        main_root=root
+        while root:
+            if root.val >val:
+                #what if the node leaf are empty
+                if root.left:
+                    root=root.left
+                else:
+                    #set a new node and link it
+                    node=TreeNode(val,None,None)
+                    root.left=node
+                    break
+            elif root.val<val:
+                if root.right:
+                    root=root.right
+                else:
+                    node=TreeNode(val,None,None)
+                    root.right=node
+                    break
+        return main_root
+    
+    def helper(self,root):
+        # below are the edge cases
+        if root.left is None:
+            return root.right
+        if root.right is None:
+            return root.left
+        rightNode=root.right#taking the full right
+        leftNodelastright=self.findthelast(root.left)
+        #THE LINK
+        leftNodelastright.right=rightNode
+
+        return root.left
+    
+    def findthelast(self,root):
+        if root.right is None:
+            return root
+        return self.findthelast(root.right)
+   
+
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        main_root=root
+        if root is None:
+            return None
+
+        if root.val==key:
+            return self.helper(root)
+        #we find the root first
+        # the right node if there is a link of the key right
+        # we take that right and keep a link to the last right to maintain the bst
+        while root:
+            if root.val>key:
+                if (root.left!=None and root.left.val==key):
+                    root.left=self.helper(root.left)
+                    break
+                else:
+                    root=root.left
+            else:
+                if root.right!=None and root.right.val==key:
+                    root.right=self.helper(root.right)
+                    break
+                else:
+                    root=root.right
+        return main_root
+
+
+
+
+
+
+
+
+        
         
