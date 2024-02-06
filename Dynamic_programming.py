6fcd7df71e4483b74c6233951c0843c8d9e371a3
kishore270598 <kk94323@gmail.com>
2024-02-06 23:19:36 +0530

    fibonacci_tab,climbStairs

---
+++ Dynamic_programming.py
@@ -0,0 +1,50 @@
+class Dynamic_programing:
+    def fibonacci(self,n):
+        #memory method
+        def f(n,dp):
+            if n<=1:
+                return n
+            if dp[n]!=-1:
+                return dp[n]
+            dp[n]=f(n-1,dp)+f(n-2,dp)
+            return dp[n]
+        dp=[-1]*(n+1)
+        return f(n,dp)
+    
+    def fibonacci_tab(self,n):
+        #tabulation method
+        #TIME COMPLEX- TC O(N)
+        #SPAEC COMPLEC- SC 0(1)
+        prev2=0
+        cur=0
+        prev=1
+        for i in range(2,n+1,1):
+            cur=prev2+prev
+            prev2=prev
+            prev=cur
+        return cur
+    def climbStairs(self, n):
+            # def f(n):
+            #     if n==0 or n==1:
+            #         return 1
+            #     left=f(n-1)
+            #     right=f(n-2)
+            #     return left+right
+            # return f(n)
+            prev1=1
+            prev2=1
+            if n==1 or n==0:
+                return 1
+            cur=0
+            for i in range(2,n+1,1):
+                cur=prev1+prev2
+                prev2=prev1
+                prev1=cur
+            return cur
+
+
+
+d=Dynamic_programing()
+ans=d.fibonacci_tab(5)
+print('ANS',ans)
+
