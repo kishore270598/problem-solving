cd796127b1fea4951a9d61c9b78dac3fb0758c0b
kishore270598 <kk94323@gmail.com>
2024-02-07 23:24:21 +0530

    minimizeCost,frogJump

--- Dynamic_programming.py
+++ Dynamic_programming.py
@@ -1,3 +1,4 @@
+import math
 class Dynamic_programing:
     def fibonacci(self,n):
         #memory method
@@ -41,10 +42,62 @@ class Dynamic_programing:
                 prev2=prev1
                 prev1=cur
             return cur
+  
+    def frogJump(self,n, heights):
+        # def f(n,dp):
+        #     if n==0:
+        #         return 0
+        #     if dp[n]!=-1:
+        #         return dp[n]
+        #     left=f(n-1,dp)+abs(heights[n]-heights[n-1])
+        #     right=float('inf')
+        #     if n>1:
+        #         right=f(n-2,dp)+abs(heights[n]-heights[n-2])
+        #     dp[n]=min(left,right)
+        #     return dp[n]
+        #left,right=0,0
+        #memo method
+        # dp=[-1]*(n+1)
+        # dp[0]=0
+        # for i in range(1,n+1,1):
+        #     left=dp[i-1]+abs(heights[i]-heights[i-1])
+        #     right=float('inf')
+        #     if i>1:
+        #         right=dp[i-2]+abs(heights[i]-heights[i-2])
+        #     dp[i]=min(left,right)
+        # return dp[n-1]
+        curr=0
+        prev=0
+        prev2=0
+        for i in range(1,n+1,1):
+            left=prev+abs(heights[i]-heights[i-1])
+            right=float('inf')
+            if i>1:
+                right=prev2+abs(heights[i]-heights[i-2])
+            curr=min(left,right)
+            prev2=prev
+            prev=curr
+        return curr
+
+    
+    def minimizeCost(self,n, k, heights):
+        dp=[-1]*(n)
+        dp[0]=0
+        for i in range(1,n,1):
+            min_=float('inf')
+            for j in range(1,k+1,1):
+                if (i-j>=0):# jump is valid
+                    jump=dp[i-j]+abs(heights[i]-heights[i-j])
+                    min_=min(jump,min_)
+            
+            dp[i]=min_
+        return dp[n-1]
+    
 
 
 
 d=Dynamic_programing()
-ans=d.fibonacci_tab(5)
+heights=[10, 40, 30, 10]
+ans=d.minimizeCost(4,2,heights)
 print('ANS',ans)
 
