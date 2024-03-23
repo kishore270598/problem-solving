ea227012919b9c1bf96e4269c7d917514568e224
kishore270598 <kk94323@gmail.com>
2024-03-23 19:25:35 +0530

    next permutation

--- tries.py
+++ tries.py
@@ -146,4 +146,58 @@ class Solution:
             #we need to get the correct last value
             ans=max(ans,nums ^ node.val)
         return ans 
-          
+#Maximum XOR With an Element From Array
+class Trie:
+    def __init__(self):
+        self.root={}
+        self.m=0
+    
+    def insert(self,word):
+        node=self.root
+        for ch in word:
+            if ch not in node:
+                node[ch]={}
+            node=node[ch]
+
+    def compare(self,word,x):
+        node=self.root
+        t=""
+        a,b='0','1'
+        for ch in word:
+            if ch==a and b in node:
+                t+=b
+                node=node[b]
+            elif ch==b and a in node:
+                t+=a
+                node=node[a]
+            else:
+                t+=ch
+                node=node[ch]
+        ans=int(t,2)^x
+        return ans
+
+class Solution:
+    def maximizeXor(self, nums, queries):
+        trie=Trie()
+        n,l=len(queries),len(nums)
+        for i in range(n):
+            queries[i].append(i)
+        queries.sort(key=lambda x:x[1])
+        nums.sort()
+        res=[0]*n
+        j=0
+        m=nums[0]
+        for i in range(n):
+            x,y,z=queries[i][0],queries[i][1],queries[i][2]
+            if m>y:
+                res[z]=-1
+                continue
+            while j<l and nums[j]<=y:
+                word="{:032b}".format(nums[j])
+                trie.insert(word)
+                j+=1
+            word="{:032b}".format(x)
+            ans=trie.compare(word,x)
+            res[z]=ans
+            t=y
+        return res
